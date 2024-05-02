26aec3ea0653d18228ec054211d99474ad1e6593
PhokRithy <158687728+PhokRithy@users.noreply.github.com>
2024-05-02 08:59:48 +0700

    first commit

---
+++ src/components/Card.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Card.css";
+
+const CreateCard = () => {
+    const style = {
+        color: "red",
+    };
+  return (
+    <>
+        <div className="card">This is .....</div>
+        <button>Click me</button>
+    </>
+  )
+
+};
+
+export default CreateCard;
